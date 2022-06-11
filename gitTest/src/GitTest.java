8426301540642a3973bff93c950bce1de4f9ab48
홍혁준 <jjj8337721@naver.com>
2022-06-11 17:27:18 +0900

    Test Commit

--- gitTest/src/GitTest.java
+++ gitTest/src/GitTest.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class gitTest {
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git");
+        System.out.println("This is 11 version.");
+    }
 }
