64ed2f73898568f933d7029376570f68108ccc63
홍혁준 <jjj8337721@naver.com>
2022-06-11 17:32:41 +0900

    B Update devB

--- gitTest/src/B.java
+++ gitTest/src/B.java
@@ -1,6 +1,6 @@
 public class B {
 
     public static void main(String[] args) {
-        System.out.println("B Class Update => Branch devB");
+        System.out.println("B Class Update => Branch devB Final");
     }
 }
