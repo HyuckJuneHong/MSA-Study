1adaf182b7db4d25cc304e2485131f7f66c20049
홍혁준 <jjj8337721@naver.com>
2022-06-12 23:58:41 +0900

    초기 구성 및 Eureka 설계

---
+++ MSATest/src/main/java/kr/co/msatest/MsaTestApplication.java
@@ -0,0 +1,13 @@
+package kr.co.msatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaTestApplication.class, args);
+    }
+
+}
